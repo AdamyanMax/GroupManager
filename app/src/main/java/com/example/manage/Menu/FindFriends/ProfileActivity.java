9b80b60da89461b945891c4b219ca6b2637686da
MaximAdamyan <maximadamyan2@gmail.com>
2023-04-02 17:15:12 +0400

    Commit #7 Clicking the profile in the FindFriendsActivity RecyclerView now opens a profile for each user

--- app/src/main/java/com/example/manage/Menu/FindFriends/ProfileActivity.java
+++ app/src/main/java/com/example/manage/Menu/FindFriends/ProfileActivity.java
@@ -1,23 +1,76 @@
 package com.example.manage.Menu.FindFriends;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.example.manage.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private String receiverUserID;
+
+    private CircleImageView civProfileImage;
+    private TextView tvUsername, tvUserStatus;
+    private Button btnSendMessageRequest;
+
+    private DatabaseReference UserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         receiverUserID = getIntent().getExtras().get("visit_user_id").toString();
 
-        Toast.makeText(this, "User ID: " + receiverUserID, Toast.LENGTH_SHORT).show();
+        civProfileImage = findViewById(R.id.civ_profile_user_image);
+        tvUsername = findViewById(R.id.tv_profile_username);
+        tvUserStatus = findViewById(R.id.tv_profile_user_status);
+        btnSendMessageRequest = findViewById(R.id.btn_send_message_request);
+
+        retrieveUserInfo();
+    }
+
+    private void retrieveUserInfo() {
+        UserRef.child(receiverUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if ((snapshot.exists()) && (snapshot.hasChild("image"))) {
+                    String userImage = Objects.requireNonNull(snapshot.child("image").getValue()).toString();
+                    String userName = Objects.requireNonNull(snapshot.child("name").getValue()).toString();
+                    String userStatus = Objects.requireNonNull(snapshot.child("status").getValue()).toString();
+
+                    Picasso.get().load(userImage).placeholder(R.drawable.user_default_profile_pic).into(civProfileImage);
+                    tvUsername.setText(userName);
+                    tvUserStatus.setText(userStatus);
+                } else if (snapshot.exists()){
+                    String userName = Objects.requireNonNull(snapshot.child("name").getValue()).toString();
+                    String userStatus = Objects.requireNonNull(snapshot.child("status").getValue()).toString();
+
+                    tvUsername.setText(userName);
+                    tvUserStatus.setText(userStatus);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
